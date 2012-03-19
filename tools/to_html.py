5dbb1122a3f14b0892eff7555bbd5dcd011bf564
Kaarel Kaljurand <kaljurand@gmail.com>
2012-03-19 11:37:54 +0100

    Nicer HTML output

--- tools/to_html.py
+++ tools/to_html.py
@@ -3,18 +3,53 @@
 # Formats the output of info.py as HTML with links to AceWiki.
 
 # Author: Kaarel Kaljurand
-# Version: 2012-03-16
+# Version: 2012-03-19
 #
 # Examples:
 #
 # python info.py -n "http://cloud.grammaticalframework.org" -p 80 -d "/grammars/" |\
-# python to_html.py -n "http://cloud.grammaticalframework.org" -p 80 -d "/grammars/"
+# python to_html.py -n "http://localhost" -p 9077 -t "Links to AceWiki"
 
+import datetime
 import sys
 import argparse
 import os
 import re
 from string import Template
+
+template_header = Template("""<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<title>${name}</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+<style type="text/css">
+h1, h2, h3, h4, h5, h6 { font-family: Optima, sans-serif }
+h1 { border-bottom: 1px solid black }
+pre { padding: 1em 1em 1em 1em; background-color: #eee; border: 1px solid silver }
+address { border-top: 1px solid black; padding-top: 0.4em }
+body { font-family: Verdana, Arial, sans-serif }
+</style>
+</head>
+
+<body>
+
+<h1>
+<img src="/AceWikiLogoSmall.png" alt="AceWiki"/>
+${name}</h1>
+""")
+
+template_footer = Template("""
+<address>
+Created at: ${timestamp}
+</address>
+
+</body>
+</html>
+""")
 
 template_grammar = Template("""
   <h2>${grammar}</h2>
@@ -80,9 +115,20 @@ parser.add_argument('-p', '--port', type=int, action='store',
 					dest='server_port',
 					help='port of AceWiki')
 
+parser.add_argument('-t', '--title', type=str, action='store',
+					default='',
+					dest='page_title',
+					help='title of the generated page')
+
 parser.add_argument('-v', '--version', action='version', version='%(prog)s v0.1')
 
 args = parser.parse_args()
 
 server = args.server_name + ":" + str(args.server_port)
+print template_header.substitute(
+	name = args.page_title
+)
 format(server)
+print template_footer.substitute(
+	timestamp = datetime.datetime.now()
+)
